9ff7b8cc31b0daef68e41fcfa81b85ff1fe0eabf
Aarav414 <84316227+Aarav414@users.noreply.github.com>
2021-12-03 18:03:42 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -17,7 +17,28 @@ function take_snapshot()
     }
 
    console.log('ml5 version', ml5.version);
-   classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/[...]model.json',modelLoaded)
+   classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/[...]model.json',modelLoaded);
+   function modelLoaded()
+       {
+           console.log('Model Loaded!');
+       }
+   
+
+   function check()
+   {
+       img=document.getElementById('captured_image');
+       classifier.classify(img, gotresult);
+   }
+
+   function gotresult(error,results) {
+       if(error) {
+           console.error(error);
+       } else {
+           console.log(results)
+           document.getElementById('result_object_name').innerHTML=results[0].label;
+           document.getElementById("]'result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+       }
+   }
 
     
 
